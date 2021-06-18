334fcb6672ea3935c8b1f707c5f2d049a66b8b67
muhammed sabique <18bl12313@rit.ac.in>
2021-06-18 21:17:17 +0530

    update by center name

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import time
 
 from datetime import date
 
+from requests.models import Response
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 
 def getDistrictId():
@@ -53,31 +55,58 @@ def playAmbulanceSound():
     from playsound import playsound
     playsound("ambulance.mp3")
     
-def liveNotify(district_id,date,age):
+def liveNotify(district_id,date):
     while True:
         response = requests.get(f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={date}', headers=headers)
         response_json =  response.json()
         #print(response_json)
         for center in response_json['centers']:
             for session in center['sessions']:
-                if session['available_capacity_dose1'] != 0 and int(session['min_age_limit'])<=age:
+                if session['available_capacity_dose1'] != 0 and int(session['min_age_limit']):
                     playAmbulanceSound()
                     print(f"{center['name']}--{center['fee_type']}")
                     print(f"      {session['date']} -- {session['available_capacity_dose1']} --min age:{session['min_age_limit']}")
         time.sleep(60*2)
         print("...")
-
-
-
+def notifyByCenterName(district_id,date,center_list,age_limit):
+    print_ = True
+    while True:
+        def api_call():   
+            try:   
+                response = requests.get(f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={district_id}&date={date}', headers=headers)
+                response_json =  response.json()
+                return response_json
+            except:
+               print("[Reconnecting..]") 
+               time.sleep(20)
+               api_call()
+               
+        response_json= api_call()       
+        if print_:
+                print("[Finding Dose at...]")
+        for center in response_json['centers']:
+            if center['name'] in center_list:
+                if print_:
+                    print(f"    {center['name']}")
+                for session in center['sessions']:
+                    if session['available_capacity_dose1'] != 0 and int(session['min_age_limit']==age_limit):
+                        print(f"[Dose avilable]...  \n      {center['name']}--{center['fee_type']}")
+                        print(f"      {session['date']} -- {session['available_capacity_dose1']} --min age:{session['min_age_limit']}")
+                        playAmbulanceSound()
+        time.sleep(60*2)
+        print("[REFRESHING]...")
+        print_=False
 def main():
     getDistrictId()
     district_id = int(input("enter your District Id: "))
     today = getTodayDate()
-    #getCenterByDistrict(district_id,today)
-    print("##############################################################################################################")
-    getAvilableCenterByDistrict(district_id,today)
-    age = int(input("To get live notification enter your age: "))
-    print("[LIVE NOTIFY IS ACTIVE...]")
-    liveNotify(district_id, today, age)
-    
+    # getCenterByDistrict(district_id,today)
+
+    # print("##############################################################################################################")
+    # #305etAvilableCenterByDistrict(district_id,today)
+    # print("[LIVE NOTIFY IS ACTIVE...]")
+    # #liveNotify(district_id, today)
+    center_list = ["Madavoor FHC", "Narikkuni CHC", "Koduvally CHC"]
+    notifyByCenterName(district_id,today,center_list,40)
 main()
+
